988259eeb2f419010b89af913a0be9d527e000e6
arazon005 <arazon005@gmail.com>
2024-11-15 22:04:43 +0000

    working on functionality

--- main.js
+++ main.js
@@ -2,12 +2,36 @@ var $form = document === null || document === void 0 ? void 0 : document.querySe
 var $addNew = document === null || document === void 0 ? void 0 : document.querySelector('.add');
 var $modal = document === null || document === void 0 ? void 0 : document.querySelector('dialog');
 var $cancelButton = document === null || document === void 0 ? void 0 : document.querySelector('.cancel');
-var $confirmButton = document === null || document === void 0 ? void 0 : document.querySelector('.confirm');
+var index = 1;
 $addNew.addEventListener('click', function () {
     $modal.showModal();
 });
 $cancelButton.addEventListener('click', function () {
     $modal.close();
 });
-$form.addEventListener('submit', function () {
+$form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log('event fired');
+    var $tableRow = document.querySelectorAll('.table-row');
+    var $formElement = $form.elements;
+    var newEntry = {
+        time: $formElement.time.value,
+        day: $formElement.day.value,
+        event: $formElement.eventInfo.value,
+        id: index,
+    };
+    for (var i = 0; i < $tableRow.length; i++) {
+        if ($tableRow[i].getAttribute('id') === String(1)) {
+            while ($tableRow[i].firstChild) {
+                $tableRow[i].firstChild.remove();
+            }
+            renderEvent($tableRow[i], newEntry);
+        }
+    }
+    index++;
 });
+function renderEvent(row, entry) {
+    var $timeCell = document.createElement('td');
+    $timeCell.textContent = entry.time;
+    row.appendChild($timeCell);
+}
--- main.ts
+++ main.ts
@@ -2,14 +2,21 @@ const $form = document?.querySelector('form');
 const $addNew = document?.querySelector('.add');
 const $modal = document?.querySelector('dialog');
 const $cancelButton = document?.querySelector('.cancel');
-const $confirmButton = document?.querySelector('.confirm');
+// const $confirmButton = document?.querySelector('.confirm');
 
 interface FormElements extends HTMLFormControlsCollection {
   time: HTMLSelectElement;
   day: HTMLSelectElement;
-  event: HTMLTextAreaElement;
+  eventInfo: HTMLTextAreaElement;
 }
 
+interface Entry {
+  time: string;
+  day: string;
+  event: string;
+  id: number;
+}
+let index: number = 1;
 $addNew.addEventListener('click', () => {
   $modal.showModal();
 });
@@ -18,15 +25,32 @@ $cancelButton.addEventListener('click', () => {
   $modal.close();
 });
 
-
-
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
+  console.log('event fired');
   const $tableRow = document.querySelectorAll('.table-row');
 
   const $formElement = $form.elements as FormElements;
+  const newEntry = {
+    time: $formElement.time.value,
+    day: $formElement.day.value,
+    event: $formElement.eventInfo.value,
+    id: index,
+  };
 
   for (let i = 0; i < $tableRow.length; i++) {
-    if ($tableRow[i].getAttribute('id') === )
+    if ($tableRow[i].getAttribute('id') === String(1)) {
+      while ($tableRow[i].firstChild) {
+        $tableRow[i].firstChild.remove();
+      }
+      renderEvent($tableRow[i], newEntry);
+    }
   }
+  index++;
 });
+
+function renderEvent(row: Element, entry: Entry): void {
+  const $timeCell = document.createElement('td');
+  $timeCell.textContent = entry.time;
+  row.appendChild($timeCell);
+}
